3b1955b124fd1fdf6c3801805c265dcadfd4be41
Gadds23 <119502158+Gadds23@users.noreply.github.com>
2023-08-07 00:34:31 +0800

    Adjust SQL

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3.8.3',
+        'NAME': BASE_DIR / 'db.sqlite3.8.3',
     }
 }
 
